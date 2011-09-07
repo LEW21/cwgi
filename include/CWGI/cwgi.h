8a818f5b1c0a074a97d3215444761e4b76ff34c0
Janusz Lewandowski <lew21@xtreeme.org>
2011-09-07 16:08:06 +0200

    Replace the sequential reading of headers with a map. Add CWGI_ApplicationFactory - macro for compiling CWGI Apps as libraries.

--- include/CWGI/cwgi.h
+++ include/CWGI/cwgi.h
@@ -1,31 +1,22 @@
 #ifndef CWGI_cwgi_h
 #define CWGI_cwgi_h
 
+#include <map>
 #include <string>
 #include <functional>
 #include <vector>
 
 namespace CWGI
 {
-	struct Header
-	{
-		std::string name;
-		std::string value;
-
-		Header() = default;
-
-		Header(const std::string& n, const std::string& v): name(n), value(v) {}
-		Header(std::string&& n, std::string&& v): name(std::move(n)), value(std::move(v)) {}
-
-		bool empty() const {return name.empty();}
-	};
+	typedef std::pair<std::string, std::string> Header;
+	typedef std::map<std::string, std::string> Headers;
 
 	struct Request
 	{
 		Request();
 		virtual ~Request();
 
-		virtual Request& operator>>(Header& header) = 0;
+		virtual Request& operator>>(Headers& headers) = 0;
 		virtual Request& operator>>(std::string& data) = 0;
 	};
 
@@ -34,13 +25,17 @@ namespace CWGI
 		Response();
 		virtual ~Response();
 
-		virtual Response& operator<<(const Header& header) = 0;
+		virtual Response& operator<<(const Headers& headers) = 0;
 		virtual Response& operator<<(const std::string& data) = 0;
 	};
 
 	typedef std::function<void (Request&, Response&)> Application;
 
 	Application chain(const std::vector<Application>&);
+
+	typedef Application* (*ApplicationFactory)();
 }
+
+#define CWGI_ApplicationFactory extern "C" CWGI::Application* CWGI_newApplication()
 
 #endif
