2b44a3ce3cfbbacdf614deb8ff5b97241664aecd
Janusz Lewandowski <lew21@xtreeme.org>
2011-10-01 21:52:21 +0200

    Fix MessageBuffer

--- src/MessageBuffer.h
+++ src/MessageBuffer.h
@@ -8,13 +8,13 @@ namespace CWGI
 {
 	class MessageBuffer: public Response, public Request
 	{
-		ATL::SyncQueue<Header> h;
+		ATL::SyncQueue<Headers> h;
 		ATL::SyncQueue<std::string> d;
 
 	public:
-		inline Response& operator<<(const Header& header) { h.push(header); return *this; }
+		inline Response& operator<<(const Headers& headers) { h.push(headers); return *this; }
 		inline Response& operator<<(const std::string& data) { d.push(data); return *this; }
-		inline Request& operator>>(Header& header) { header = h.pop(); return *this; }
+		inline Request& operator>>(Headers& headers) { headers = h.pop(); return *this; }
 		inline Request& operator>>(std::string& data) { data = d.pop(); return *this; }
 	};
 }
